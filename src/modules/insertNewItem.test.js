a3dacd692945d98b6945a6a74262559746f4b63e
Vitor Guedes <vgm_rox@hotmail.com>
2022-07-13 19:11:19 -0300

    Add more modules

--- src/modules/insertNewItem.test.js
+++
@@ -1,7 +0,0 @@
-import { insertNewItem } from './insertNewItem.js';
-import DataClass from './creatingItems.js';
-const { dataStructure } = DataClass;
-
-test('Return and obj with 3 properties', () => {
-  expect(insertNewItem('karla')).toBe({description: 'karla', completed: false, index: dataStructure.length});
-});
